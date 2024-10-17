8b3a77dd9b4d56984940a29d8e820b0795c0072c
Marat Davudov <marat.davudov@sky.uk>
2024-10-17 16:53:51 +0100

    Initial commit

---
+++ swarm/types.py
@@ -0,0 +1,44 @@
+from openai.types.chat import ChatCompletionMessage
+from openai.types.chat.chat_completion_message_tool_call import (
+    ChatCompletionMessageToolCall,
+    Function,
+)
+from typing import List, Callable, Union, Optional
+
+# Third-party imports
+from pydantic import BaseModel
+from typing import Type
+
+AgentFunction = Callable[[], Union[str, "Agent", dict]]
+
+
+class Agent(BaseModel):
+    name: str = "Agent"
+    model: str = "gpt-4o"
+    instructions: Union[str, Callable[[], str]] = "You are a helpful agent."
+    functions: List[AgentFunction] = []
+    tool_choice: str = None
+    parallel_tool_calls: bool = True
+    response_format: Union[Type[BaseModel], None] = None # Attaching response format to an agent
+
+
+
+class Response(BaseModel):
+    messages: List = []
+    agent: Optional[Agent] = None
+    context_variables: dict = {}
+
+
+class Result(BaseModel):
+    """
+    Encapsulates the possible return values for an agent function.
+
+    Attributes:
+        value (str): The result value as a string.
+        agent (Agent): The agent instance, if applicable.
+        context_variables (dict): A dictionary of context variables.
+    """
+
+    value: str = ""
+    agent: Optional[Agent] = None
+    context_variables: dict = {}
